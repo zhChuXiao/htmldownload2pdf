ca68cf610ba83c58d3127963f88f1650316be2a8
ChuXiao <zh.chuxiao@gmail.com>
2023-12-18 22:21:48 +0800

    feat: 优化

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import JsPDF from 'jspdf'
  *
  * @param el 需要截图的包裹的（原生的）DOM 对象
  * @param fileName 文件名称
- * @description 将html元素转换成canvas，再将canvas转换成pdf下载
+ * @description 将html元素转换成canvas，再将canvas转换成pdf下载，适用于vue2和vue3以及原生js
  * @example printOut(document.getElementById('app')!, 'test')
  */
 export function printOut(el: HTMLElement, fileName: string) {
